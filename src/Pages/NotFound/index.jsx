93e52a49fea892cf81a80ef72ab14db7f9ac3459
Johanna Apure Canónico <japurecanonico@gmail.com>
2023-12-06 15:12:57 -0400

    update

--- src/Pages/NotFound/index.jsx
+++ src/Pages/NotFound/index.jsx
@@ -1,7 +1,12 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+
 function NotFound() {
-  return <div className="bg-pink-100">
-    Not Found
-  </div>;
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-3">
+      <MagnifyingGlassIcon className="w-20 h-20" />
+      <p className="w-full text-center text-xl">Pagina no encontada</p>
+    </div>
+  );
 }
 
 export default NotFound;
