785d8fd0d483daacc3ba9ff57ff476a7314e1f9b
Johanna Apure Canónico <japurecanonico@gmail.com>
2024-02-21 17:14:36 -0400

    optimizar header como componente

--- zaitax_home/js/script.js
+++ zaitax_home/js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function() { 
 	$(".list-services a.tooltips").easyTooltip();
+
+	// componentes
+	$("#header").load('header.html');
+
+	switch (key) {
+		case value:
+			$("#nav-services").addClass("active")
+			break;
+	
+		default:
+			break;
+	}
 }); 
